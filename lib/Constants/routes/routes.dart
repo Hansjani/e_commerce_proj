f6a527d5aebfe506277a206a67b8f292765aeefb
hansjani <hansjani2002@gmail.com>
2024-04-02 18:36:54 +0530

    test

--- lib/Constants/routes/routes.dart
+++
@@ -1,5 +0,0 @@
-const mainPageRoute = '/main_page/';
-const userProfileRoute = '/userProfile/';
-const userLoginRoute = '/userLogin/';
-const userRegisterRout = '/userRegister/';
-const testUserProfile = '/userTestProfile/';
