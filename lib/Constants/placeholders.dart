8f38eff554e0a01085044bb07a0848dab51f9f7e
hansjani <hansjani2002@gmail.com>
2024-04-27 13:09:33 +0530

    update-11.test

--- lib/Constants/placeholders.dart
+++
@@ -1,4 +0,0 @@
-class PlaceHolderImages{
-  static const itemPlaceholder = "http://laptop-16-e0xxx.local/app_db/server_file/items/placeholder_image.jpeg";
-  static const ip = 'laptop-16-e0xxx.local';
-}
